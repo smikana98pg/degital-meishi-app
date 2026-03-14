6417d7db3e393827c114f1800d96938b097772c4
Kaname Sudo <mikan.paku89@gmail.com>
2026-03-14 18:35:42 +0900

    初期画面作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
-import "./App.css";
+import { Box, Button, Field, Flex, Heading, Input } from "@chakra-ui/react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router";
 
 function App() {
+  const [id, setId] = useState("");
+  const navigate = useNavigate();
+
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <Flex justify="center" align="center" minH="100vh">
+      <Box textAlign="center">
+        <Heading as="h1" size="3xl" mb={6}>
+          デジタル名刺アプリ
+        </Heading>
+        <Box bg="white" rounded="lg" shadow="md" p={6} w="320px">
+          <Field.Root>
+            <Field.Label>ID</Field.Label>
+            <Input value={id} onChange={(e) => setId(e.target.value)} />
+            <Button
+              colorPalette="teal"
+              fontWeight="bold"
+              width="full"
+              mt={2}
+              onClick={() => navigate(`/cards/${id}`)}
+            >
+              名刺をみる
+            </Button>
+          </Field.Root>
+        </Box>
+        <Link to="/">
+          <Box mt={3} fontSize="sm" color="gray.700">
+            新規登録はこちら
+          </Box>
+        </Link>
+      </Box>
+    </Flex>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "@/components/ui/provider";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App.tsx";
+import CardPage from "./pages/CardPage.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/cards/:id",
+    element: <CardPage />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Provider>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>,
-)
+);
