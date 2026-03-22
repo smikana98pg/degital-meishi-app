986d0ff3c8b13b727942137d3f758a6e6876cfb0
Kaname Sudo <mikan.paku89@gmail.com>
2026-03-22 22:15:21 +0900

    バッチファイル修正

--- batch/index.ts
+++ batch/index.ts
@@ -1,11 +1,6 @@
 // dotenv/config を最初に import することで、他のモジュールより先に .env が読み込まれる
 import "dotenv/config";
-// import { supabase } from "../src/utils/supabase";
-import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = process.env.VITE_SUPABASE_URL as string;
-const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY as string;
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+import { supabase } from "../src/utils/supabase";
 
 // 前日作成したusersとuser_skillを削除する
 const deleteUser = async () => {
